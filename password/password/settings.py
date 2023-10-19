c7a264c359b9ecc4915359b8e05db24351b7aebe
Артемий Бутырин <artemii.butyrin@yandex.ru>
2023-10-19 22:13:36 +0300

    changes

--- password/password/settings.py
+++ password/password/settings.py
@@ -8,7 +8,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-# TODO: перенести KEY в .env
 KEY = os.getenv("KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
